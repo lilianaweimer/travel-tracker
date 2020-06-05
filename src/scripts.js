d8b902f39d6e3f6e43e544def5310629881e3529
Liliana Weimer <lilianaweimer11@gmail.com>
2020-06-05 14:59:47 -0600

    Add scripts-test file (might not need) and add fetches

--- src/scripts.js
+++ src/scripts.js
@@ -5,17 +5,26 @@ import TravelAgent from './TravelAgent';
 import domUpdates from '../src/domUpdates';
 import fetch from 'node-fetch';
 
-fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/45')
-        .then(response => console.log(response.json())),
+let user;
+let destinations;
+let trips;
+let loginButton = document.querySelector('.login-button');
 
+loginButton.addEventListener('click', domUpdates.attemptLogin())
 
 Promise.all([
-    // fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/45')
-    //     .then(response => console.log(response.json())),
-    // fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips')
-    //     .then(response => response.json()),
-    // fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
-    //     .then(response => response.json())
+    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/45')
+        .then(response => response.json()),
+    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips')
+        .then(response => response.json()),
+    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
+        .then(response => response.json())
 ])
-    // .then(data => console.log(data))
-    // .catch(err => console.error(err))
+    .then(data => createDatasets(data[0], data[1], data[2]))
+    .catch(err => console.error(err))
+
+const createDatasets = (userData, destinationData, tripsData) => {
+
+    destinations = destinationData;
+    trips = tripsData;
+}
